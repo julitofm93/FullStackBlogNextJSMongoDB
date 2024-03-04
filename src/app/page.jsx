959579724fa7b3d01a7f9c26739d576f8a71fc6a
Julian <julian.fm@live.com>
2024-03-04 13:07:30 -0300

    Responsive & global css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,19 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured/Featured";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/menu/Menu";
 
 export default function Home() {
-  return <div>
-    <Link href="/">Hello</Link>
-  </div>;
+  return (
+    <div className={styles.container}>
+      <Featured/>
+      <CategoryList/>
+      <div className={styles.container}>
+        <CardList/>
+        <Menu/>
+      </div>
+    </div>
+  );
 }
