d8e8b5819850fb0dc204fb5d0adf750807543c34
julitofm93 <julian.fm@live.com>
2024-03-13 18:23:07 -0300

    Starting darkmode

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+"use client"
+
+import Image from 'next/image'
 import styles from "./themeToggle.module.css"
+import { useContext } from 'react'
+import { ThemeContext } from '@/context/ThemeContext'
 
 const ThemeToggle = () => {
+
+  const {theme} = useContext(ThemeContext);
+
+  console.log(theme)
+
   return (
-    <div className={styles.container}>ThemeToggle</div>
+    <div className={styles.container}>
+      <Image src="/moon.png" alt='' width={14} height={14}/>
+      <div className={styles.ball}></div>
+      <Image src="/sun.png" alt='' width={14} height={14}/>
+    </div>
   )
 }
 
