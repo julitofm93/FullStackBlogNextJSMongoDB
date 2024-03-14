3179bad1aa862bc911d1bea5e8fe27df9822c804
julitofm93 <julian.fm@live.com>
2024-03-14 13:15:18 -0300

    burger menu

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { createContext, useState } from "react"
+import { createContext, useEffect, useState } from "react"
 
 export const ThemeContext = createContext();
 
 const getFromLocalStorage = () => {
-    if (typeof window !== undefined) {
+    if (typeof window !== "undefined") {
       const value = localStorage.getItem("theme");
       return value || "light";
     }
@@ -15,8 +15,17 @@ export const ThemeContextProvider = ({ children }) => {
     const [theme, setTheme] = useState(()=>{
         return getFromLocalStorage()
     });
+
+    const toggle = () =>{
+        setTheme(theme === "light" ? "dark" : "light")
+    };
+
+    useEffect(()=>{
+        localStorage.setItem("theme",theme)
+    }, [theme]);
+
     return (
-        <ThemeContext.Provider value={{theme}}>
+        <ThemeContext.Provider value={{theme, toggle}}>
             {children}
         </ThemeContext.Provider>
     );
